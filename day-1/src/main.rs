85e9d89d5d65e2b4f2057583c3d98969dbe100f0
Chuck Dries <chuck@chuckdries.com>
2022-12-01 13:31:15 -0800

    add fixed for loop implementation

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,32 +1,28 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
+#[allow(dead_code)]
 #[derive(Debug)]
 struct DayOneAnswers {
     max_sum: usize,
     top_three_sum: usize,
 }
 
+fn get_reader() -> BufReader<File> {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    reader
+}
+
 fn main() {
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
-    
-    let chuck_answers = chuck_fold(reader);
-    dbg!(chuck_answers);
-    /* chuck_answers = DayOneAnswers {
-        max_sum: 69206,
-        top_three_sum: 197400,
-    } */
+    let chuck_fold_answers = chuck_fold(get_reader());
+    dbg!(chuck_fold_answers);
 
-    let file = File::open("input.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    let jaclyn_answers = jaclyn_for(reader);
+    let jaclyn_answers = jaclyn_for(get_reader());
     dbg!(jaclyn_answers);
-    /* jaclyn_answers = DayOneAnswers {
-        max_sum: 69206,
-        top_three_sum: 191554,
-    } */
+
+    let chuck_for_answers = chuck_for(get_reader());
+    dbg!(chuck_for_answers);
 }
 
 fn jaclyn_for(reader: BufReader<File>) -> DayOneAnswers {
@@ -83,6 +79,39 @@ fn chuck_fold(reader: BufReader<File>) -> DayOneAnswers {
             }
         },
     );
+
+    let top_three_sum = max_sum + second_sum + third_sum;
+
+    DayOneAnswers {
+        max_sum,
+        top_three_sum,
+    }
+}
+
+fn chuck_for(reader: BufReader<File>) -> DayOneAnswers {
+    let mut current_sum = 0;
+    let mut max_sum = 0;
+    let mut second_sum = 0;
+    let mut third_sum = 0;
+
+    for wrapped in reader.lines() {
+        let line = wrapped.unwrap();
+        if line == "" {
+            if current_sum > max_sum {
+                third_sum = second_sum;
+                second_sum = max_sum;
+                max_sum = current_sum;
+            } else if current_sum > second_sum {
+                third_sum = second_sum;
+                second_sum = current_sum;
+            } else if current_sum > third_sum {
+                third_sum = current_sum;
+            }
+            current_sum = 0;
+        } else {
+            current_sum = current_sum + line.parse::<usize>().unwrap();
+        }
+    }
 
     let top_three_sum = max_sum + second_sum + third_sum;
 
