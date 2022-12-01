679a448008b0ed0118fb5260bc1f25e8feea7fad
Chuck Dries <chuck@chuckdries.com>
2022-12-01 13:22:14 -0800

    add comparison with jclyn's solution

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,10 +1,65 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
+#[derive(Debug)]
+struct DayOneAnswers {
+    max_sum: usize,
+    top_three_sum: usize,
+}
+
 fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
+    
+    let chuck_answers = chuck_fold(reader);
+    dbg!(chuck_answers);
+    /* chuck_answers = DayOneAnswers {
+        max_sum: 69206,
+        top_three_sum: 197400,
+    } */
 
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let jaclyn_answers = jaclyn_for(reader);
+    dbg!(jaclyn_answers);
+    /* jaclyn_answers = DayOneAnswers {
+        max_sum: 69206,
+        top_three_sum: 191554,
+    } */
+}
+
+fn jaclyn_for(reader: BufReader<File>) -> DayOneAnswers {
+    let mut current_sum = 0;
+    let mut max_sum = 0;
+    let mut second_sum = 0;
+    let mut third_sum = 0;
+
+    for wrapped in reader.lines() {
+        let line = wrapped.unwrap();
+        if line == "" {
+            current_sum = 0;
+        } else {
+            current_sum = current_sum + line.parse::<usize>().unwrap();
+            if current_sum > max_sum {
+                max_sum = current_sum;
+            } else if current_sum > second_sum {
+                second_sum = current_sum;
+            } else if current_sum > third_sum {
+                third_sum = current_sum;
+            }
+        }
+    }
+
+    let top_three_sum = max_sum + second_sum + third_sum;
+
+    DayOneAnswers {
+        max_sum,
+        top_three_sum,
+    }
+}
+
+fn chuck_fold(reader: BufReader<File>) -> DayOneAnswers {
     let (max_sum, second_sum, third_sum, _) = reader.lines().fold(
         (0, 0, 0, 0),
         |(max_sum, second_sum, third_sum, current), wrapped| {
@@ -29,11 +84,10 @@ fn main() {
         },
     );
 
-    println!("highest calories: {max_sum}");
+    let top_three_sum = max_sum + second_sum + third_sum;
 
-    println!("sums: {max_sum} {second_sum} {third_sum}");
-
-    let top_three = max_sum + second_sum + third_sum;
-
-    println!("top three calories: {}", top_three);
+    DayOneAnswers {
+        max_sum,
+        top_three_sum,
+    }
 }
