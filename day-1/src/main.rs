ef9328c98037b92a8162821577b8785e840d673e
Chuck Dries <chuck@chuckdries.com>
2022-12-01 12:01:17 -0800

    format doc

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -2,27 +2,32 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 fn main() {
-
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let (max_sum, second_sum, third_sum, _) = reader
-        .lines()
-        .fold((0, 0, 0, 0), |(max_sum, second_sum, third_sum, current), wrapped| {
+    let (max_sum, second_sum, third_sum, _) = reader.lines().fold(
+        (0, 0, 0, 0),
+        |(max_sum, second_sum, third_sum, current), wrapped| {
             let line = wrapped.unwrap();
             if line == "" {
                 if current > max_sum {
-                    return (current, second_sum, third_sum, 0)
+                    return (current, second_sum, third_sum, 0);
                 } else if current > second_sum {
-                    return (max_sum, current, third_sum, 0)
+                    return (max_sum, current, third_sum, 0);
                 } else if current > third_sum {
-                    return (max_sum, second_sum, current, 0)
+                    return (max_sum, second_sum, current, 0);
                 }
                 (max_sum, second_sum, third_sum, 0)
             } else {
-                (max_sum, second_sum, third_sum, current + line.parse::<usize>().unwrap())
+                (
+                    max_sum,
+                    second_sum,
+                    third_sum,
+                    current + line.parse::<usize>().unwrap(),
+                )
             }
-        });
+        },
+    );
 
     println!("highest calories: {max_sum}");
 
